974275e7218869e10082f34caa78269325c5043d
Jacob Matini <32759134+JacobCyrusStark@users.noreply.github.com>
2018-04-11 03:27:04 -0700

    Create PythonLoops.py

---
+++ Introduction/PythonLoops.py
@@ -0,0 +1,14 @@
+'''
+Title     : Python If-Else
+Subdomain : Introduction
+Domain    : Python
+Author    : Jacob Matini
+Created   : 11 April 2018
+'''
+
+if __name__ == '__main__':
+    
+    n = int(input())
+    
+    for i in range(n):
+        print(i**2)
