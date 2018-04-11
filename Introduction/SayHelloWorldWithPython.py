1e755fb0d1adfe579b6e011a3d78c5b9c1774dd2
Jacob Matini <32759134+JacobCyrusStark@users.noreply.github.com>
2018-04-11 01:25:17 -0700

    Create SayHelloWorldWithPython.py
    First challenge
    Very easy
    5 points

---
+++ Introduction/SayHelloWorldWithPython.py
@@ -0,0 +1,10 @@
+'''
+Title     : Say Hello, World! With Python
+Subdomain : Introduction
+Language  : Python
+Author    : Jacob Matini
+Created   : 04 April 2018
+'''
+
+if __name__ == '__main__':
+    print "Hello World!"
