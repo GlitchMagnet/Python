c8ff0b68cb83c22b9de91493b7ae2f441cf11735
Jacob Matini <32759134+JacobCyrusStark@users.noreply.github.com>
2018-04-11 16:48:24 -0700

    Update ListComprehensions.py

--- Introduction/ListComprehensions.py
+++ Introduction/ListComprehensions.py
@@ -1,3 +1,10 @@
+'''
+Domain    : Python
+Subdomain : Introduction
+Author    : Jacob Matini
+Created   : 11 April 2018
+'''
+
 if __name__ == '__main__':
     
     x, y, z, n = (int(input()) for _ in range(4))   
