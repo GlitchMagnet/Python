98fcd2edc60c4d6c0674105b288585c10fab7dbe
Jacob Matini <32759134+JacobCyrusStark@users.noreply.github.com>
2018-04-11 03:09:30 -0700

    Create PythonDivision.py

---
+++ Introduction/PythonDivision.py
@@ -0,0 +1,15 @@
+'''
+Title     : Python If-Else
+Subdomain : Introduction
+Domain    : Python
+Author    : Jacob Matini
+Created   : 11 April 2018
+'''
+
+if __name__ == '__main__':
+
+    a = int(input())
+    b = int(input())
+
+    print(a // b)
+    print(a / b)
