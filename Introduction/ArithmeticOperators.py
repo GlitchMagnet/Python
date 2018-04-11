ab33883a425dd737d606f5491e656a51b10ddd73
Jacob Matini <32759134+JacobCyrusStark@users.noreply.github.com>
2018-04-11 02:59:55 -0700

    Create ArithmeticOperators.py

---
+++ Introduction/ArithmeticOperators.py
@@ -0,0 +1,18 @@
+'''
+Title     : Python If-Else
+Subdomain : Introduction
+Domain    : Python
+Author    : Jacob Matini
+Created   : 11 April 2018
+'''
+
+if __name__ == '__main__':
+   
+    a = int(input())
+    b = int(input())
+    
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    
+    
