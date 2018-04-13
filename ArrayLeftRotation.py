8ad8efb10b05ff2f144c391e3c52e30d94ab4680
Jacob Matini <32759134+JacobCyrusStark@users.noreply.github.com>
2018-04-13 12:24:41 -0700

    Create ArrayLeftRotation.py

---
+++ ArrayLeftRotation.py
@@ -0,0 +1,9 @@
+def array_left_rotation(a, n, k):
+    alist = list(a)
+    return a[k:] + a[:k]
+    
+
+n, k = map(int, input().strip().split(' '))
+a = list(map(int, input().strip().split(' ')))
+answer = array_left_rotation(a, n, k);
+print(*answer, sep=' ')
